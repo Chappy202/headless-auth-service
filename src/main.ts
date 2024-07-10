c637a91c365ce1f25833edd51ebfe1eabff4217b
JJ <jj@oxibux.com>
2024-07-10 16:20:41 +0200

    feat(security): enable for common security HTTP headers

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     snapshot: true,
   });
-  // Print all environment variables in development mode
-  if (process.env.NODE_ENV === 'development') {
-    console.log(process.env);
-  }
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
